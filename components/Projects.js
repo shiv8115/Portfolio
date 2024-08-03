93f08cb1e44d7962361a4e8ae90b8ed63d15f994
Shivam Singh <shivamkumarsingh@askgalore.com>
2024-08-03 21:35:56 +0530

    fix img error

--- components/Projects.js
+++ components/Projects.js
@@ -2,13 +2,7 @@
 
 import React from 'react'
 import kryptImg from '../public/assets/Projects/krypt.png';
-import DrawingImg from '../public/assets/Projects/drawing.png';
-import PortfolioImg from '../public/assets/Projects/portfolio.png';
-import terraBlu from '../public/assets/Projects/terrablu.png';
-import daoImg from '../public/assets/Projects/dao.png';
-import walletImg from '../public/assets/Projects/wallet.png';
-import CreditImg from '../public/assets/Projects/Credit.png';
-import PageImg from '../public/assets/Projects/Page.png';
+import terrablu from "../public/assets/Projects/terrablu.png"
 import Projectitems from './Projectitems';
 
 const Projects = () => {
@@ -19,7 +13,7 @@ const Projects = () => {
         <h2 className='py-4'>Have a look at what I've built</h2>
 
         <div className='grid md:grid-cols-2 gap-8'>
-          <Projectitems title='Greenwise Platform' backgroundImg={terraBlu} projectUrl='/greenwise' Stack="Next Js, NodeJs, Postgress, Azure, Blockchain"/>
+          <Projectitems title='Greenwise Platform' backgroundImg={terrablu} projectUrl='/greenwise' Stack="Next Js, NodeJs, Postgress, Azure, Blockchain"/>
            <Projectitems title='My Wallet' backgroundImg={kryptImg} projectUrl='/krypt' Stack="React Js, Blockchain, Solidity" />
            </div>
 
