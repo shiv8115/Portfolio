72ee136dcd4c97130efcddce080b8f178e737c14
Shivam Singh <shivamkumarsingh@askgalore.com>
2024-08-26 12:04:58 +0530

    removed commented code

--- components/Skills.js
+++ components/Skills.js
@@ -251,22 +251,6 @@ const Skills = () => {
             </div>
           </div>
 
-          {/* <h2 className='py-6 text-xl '>Problem Solving skills</h2>
-            <div className='grid grid-cols-2 lg:grid-cols-4 gap-8'>
-            <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={remix} width={64} height={64} 
-                            alt="/" 
-                        />
-                        </div>
-                        <div>
-                            <h3 className='flex flex-col items-center justify-center'>REMIX IDE</h3>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
-
           <h2 className="py-6 text-xl ">Platforms I am Familiar With.</h2>
           <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
             <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
