4fd0b65a5e47623ac159c270242ee25d69ce76cd
Shivam Singh <shivamkumarsingh@askgalore.com>
2024-08-26 12:12:41 +0530

    removed code

--- components/Skills.js
+++ components/Skills.js
@@ -185,32 +185,6 @@ const Skills = () => {
               </div>
             </div>
 
-            {/* <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={node} width={64} height={64} 
-                            alt="/" 
-                        />
-                        </div>
-                        <div>
-                            <h3 className='flex flex-col items-center justify-center'>NODEJS</h3>
-                        </div>
-                    </div>
-                </div> */}
-            {/* 
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={truff} width={64} height={64} 
-                            alt="/" 
-                        />
-                        </div>
-                        <div>
-                            <h3 className='flex flex-col items-center justify-center'>TRUFFLE</h3>
-                        </div>
-                    </div>
-                </div> */}
-
             <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
               <div className="grid grid-cols-2 gap-4 justify-center items-center">
                 <div className="m-auto">
