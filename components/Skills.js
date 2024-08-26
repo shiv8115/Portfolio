f9db4ada27ce4f2f77e3698627b62874ef73851a
Shivam Singh <shivamkumarsingh@askgalore.com>
2024-08-26 12:43:01 +0530

    remove commented code from skills components

--- components/Skills.js
+++ components/Skills.js
@@ -325,20 +325,6 @@ const Skills = () => {
             
           </div>
           </a>
-
-
-          {/* <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={javaimg} width={64} height={64} 
-                            alt="/" 
-                        />
-                        </div>
-                        <div>
-                            <h3 className='flex flex-col items-center justify-center'>JAVA SCRIPT</h3>
-                        </div>
-                    </div>
-                </div> */}
         </div>
 
         </div>
